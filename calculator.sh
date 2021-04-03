5dda070b1216d1f56d91954fe475c878cf36381f
Linux Specialist <81155650+abdulllah010@users.noreply.github.com>
2021-04-03 12:09:22 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,8 +1,27 @@
 #/bin/bash
-
 #Taking input & output from user
+clear
 
-echo "                                (Linux Specialist Youtube)"
+
+trap 'printf "\n";stop' 2
+
+banner() {
+
+
+printf "\e[1;92m  _____ _    ____ ______   ____    _    __  __ \e[0m\n"
+printf "\e[1;92m |  ___/ \  / ___| _____| / ___|  / \  |  \/  |\e[0m\n"
+printf "\e[1;92m | |_ / _ \| |   |  __|  | |     / _ \ | |\/| |\e[0m\n"
+printf "\e[1;92m |  _/ ___ \ |___| |___  | |___ / ___ \| |  | |\e[0m\n"
+printf "\e[1;92m |_|/_/   \_\____|_____|  \____/_/   \_\_|  |_|\e[0m\n"
+printf "\e[1;92m                                               \e[0m\n"
+
+printf "\e[1;77m Calculator Version 1.0 Coded by Masthan\e[0m \n"
+
+printf "\n"
+
+
+}
+
 echo "Enter your 1st Number : "
 read a
 echo "Enter your 2nd Number : "
